5e24f07512ac5914ffb659795375153228906380
youssef-omarrr <es-youssef.moh.ah2026@alexu.edu.eg>
2025-10-20 18:35:41 +0300

    losses function done

---
+++ 1_Spiral_Drawing_Models/training/losses.py
@@ -0,0 +1,49 @@
+from torch.nn import BCEWithLogitsLoss
+from torch import nn, ones_like
+
+# Weighted Binary Cross-Entropy Loss
+# -------------------------------------
+class WeightedBCE(nn.Module):
+    """
+    Weighted Binary Cross-Entropy loss for binary classification tasks.
+
+    This loss extends BCEWithLogitsLoss by applying a higher penalty
+    to false negatives (i.e., when the model predicts "Healthy" for PD cases).
+
+    Args:
+        FN_penalty (float): Multiplier for penalizing false negatives (label=1). 
+                            Higher values make the model more sensitive to PD cases.
+
+    Usage:
+        loss_fn = WeightedBCE(FN_penalty=1.5)
+        loss = loss_fn(pred, label)
+
+    Expected input:
+        pred:  Tensor of shape (N, 1): raw logits from the model.
+        label: Tensor of shape (N, 1): binary ground-truth labels (0 or 1).
+    """
+    def __init__(self,
+                FN_penalty:float = 1.5,):
+        super().__init__()
+        
+        self.FN_penalty = FN_penalty
+        self.bce  = BCEWithLogitsLoss(reduction='none')
+        
+        
+    # Forward function
+    # -----------------
+    def forward(self, pred, label):
+        # 1 calculate element-wise BCE loss
+        bce_loss = self.bce (pred, label)
+        
+        # 2. create per-sample weights
+        weights = ones_like(label)
+        
+        # 3. heavier penalty on PD (false negatives: label = 1)
+        weights[label == 1] = self.FN_penalty 
+        
+        # 4. add those weights to the original loss and average
+        weighted_loss  = (bce_loss*weights).mean()
+        
+        # 5. return loss
+        return weighted_loss 
